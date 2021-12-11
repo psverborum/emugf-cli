64adea15d5f1cf0891b26753f6458dacd9a8e9e4
ubersenpaii <nyan4garyj@gmail.com>
2021-12-11 11:00:08 +0000

    add games table printing

--- psone-gf.rb
+++ psone-gf.rb
@@ -1,7 +1,31 @@
 require 'uri'
 require 'net/http'
+require 'nokogiri'
+require 'terminal-table'
 
-uri = URI('https://vimm.net/vault/PS1')
-res = Net::HTTP.get_response(uri)
+$base_uri = 'https://vimm.net/vault/?p=list&system=PS1&q='
 
-puts res.body if res.is_a?(Net::HTTPSuccess)
+def print_table(array)
+  header = %w[Title Players Year Serial Rating]
+  array.delete(header)
+  table = Terminal::Table.new rows: array, headings: header
+  puts table
+end
+
+def print_games_table(game_name)
+  base_uri = $base_uri
+  base_uri += game_name
+
+  html     = Net::HTTP.get(URI(base_uri))
+  document = Nokogiri::HTML(html)
+
+  games_array_table = []
+
+  document.at('table').search('tr').each do |row|
+    games_array_table << row.search('th, td').map { |cell| cell.text.strip }
+  end
+
+  print_table(games_array_table)
+end
+
+print_games_table(gets)
